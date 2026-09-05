7a943a6fd7fa5a98f6a4bba7263405b870be0b46
Greenie709 <greenie709@gmail.com>
2026-09-05 19:45:42 +0530

    Initial Commit

---
+++ src/main/java/com/supagreenz/podium/PodiumApplication.java
@@ -0,0 +1,13 @@
+package com.supagreenz.podium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PodiumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PodiumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/supagreenz/podium/PodiumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.supagreenz.podium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PodiumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
